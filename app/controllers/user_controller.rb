324c4925274fe98da2649cba09cf10c105a56765
rhuwellsamano <rhuwell.samano@gmail.com>
2019-01-23 11:09:12 -0500

    views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,52 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_action :current_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    redirect_to user_path(@user)
+  end
+
+  def edit
+    # @user = user.find(params[:id])
+  end
+
+  def update
+    # @user = user.find(params[:id])
+    @user.update
+
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    # @user = user.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :slogan)
+  end
+
+# ADD THE FOLLOWING PRIVATE METHOD/ACTION BELOW
+
+  def current_user
+    @user = User.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :projects
   devise_for :users
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+
+  # root to: 'home#index'
 end
